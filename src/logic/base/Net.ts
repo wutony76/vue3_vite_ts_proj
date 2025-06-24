1914fc5ca27d0e6233f1d4ce9c8c650335fc22c2
tony.wu <tony.wu@hlwtech.com.tw>
2025-06-24 15:25:37 +0800

    feat: 新增大樂透開獎資料 按鈕效果 textShake：0.5秒震動循環 colorPulse：2秒顏色變化 glowEffect：1.5秒發光效果 neonGlow：1秒霓虹發光 floatEffect：3秒浮動效果 gradientMove：3秒漸變移動

--- src/logic/base/Net.ts
+++ src/logic/base/Net.ts
@@ -1,10 +1,17 @@
 import req from './Request'
+import { GAME } from '@/logic/lottery/tw'
 
 const CMD = {
   TEST: 'posts',
   SEARCH: 'sample_agentOutsideMemberAuthQuery',
   ADD: 'sample_agentOutsideMemberAuthAdd',
   DEL: 'sample_agentOutsideMemberAuthDel'
+}
+const API_ARGS = {
+  SUCCESS: 0
+}
+const ERR = {
+  STATUS_ERROR: 'API.STATUS錯誤'
 }
 
 function post(cmd: String, query: Object) {
@@ -14,7 +21,6 @@ function post(cmd: String, query: Object) {
     data: { cmd: `${cmd}`, ...query }
   }).exec()
 }
-
 // 外部接口请求
 function getExternal(options: { url: string }, query: Object) {
   return new req({
@@ -30,8 +36,27 @@ class API {
     this.CMD = CMD
   }
 
-  external(options: { url: string }, query: Object) {
-    return getExternal(options, query)
+  external(options: { url: string }, query: Object = {}) {
+    // console.log(options, query)
+    // return getExternal(options, query)
+    return new Promise((resolve, reject) => {
+      getExternal(options, query)
+        .then((res: any) => {
+          if (res.rtCode !== API_ARGS.SUCCESS) return reject(ERR.STATUS_ERROR)
+          const _content = res.content
+          if (!_content) return reject(ERR.STATUS_ERROR)
+
+          const out: { [key: string]: any } = {}
+          _content.lastNumberList.forEach((item: any) => {
+            out[item.gameCode] = Object.assign(item, { en: GAME[item.gameCode].en })
+          })
+          out[_content.bingo.gameCode] = Object.assign(_content.bingo, {
+            en: GAME[_content.bingo.gameCode].en
+          })
+          return resolve(out)
+        })
+        .catch(err => reject(err))
+    })
   }
 
   //__SAMPLE__
---
+++ src/logic/lottery/tw.ts
@@ -0,0 +1,88 @@
+interface GameItem {
+  code: number
+  name: string
+  en: string
+}
+
+interface GameMap {
+  [key: string]: GameItem
+  [key: number]: GameItem
+}
+
+// __API台灣彩券__
+const _WEI_LI_CAI = {
+  code: 5134,
+  name: '威力彩',
+  en: 'WEILICAI'
+}
+const _DA_LE_TOU = {
+  code: 5118,
+  name: '大樂透',
+  en: 'DALETOU'
+}
+const _JIN_CAI = {
+  code: 1197,
+  name: '今彩539',
+  en: 'JINCAI539'
+}
+const _YUE_HE_CAI_39 = {
+  code: 5120,
+  name: '39樂合彩',
+  en: 'YUEHECAI39'
+}
+const _YUE_HE_CAI_49 = {
+  code: 1121,
+  name: '49樂合彩',
+  en: 'YUEHECAI49'
+}
+const _XING_CAI_3 = {
+  code: 2108,
+  name: '3星彩',
+  en: '3XINGCAI'
+}
+const _XING_CAI_4 = {
+  code: 2109,
+  name: '4星彩',
+  en: '4XINGCAI'
+}
+const _BIN_GUO = {
+  code: 1102,
+  name: '賓果',
+  en: 'BINGUO'
+}
+
+export const GAME: GameMap = {
+  WEI_LI_CAI: _WEI_LI_CAI,
+  5134: _WEI_LI_CAI,
+  DA_LE_TOU: _DA_LE_TOU,
+  5118: _DA_LE_TOU,
+  JIN_CAI: _JIN_CAI,
+  1197: _JIN_CAI,
+  YUE_HE_CAI_39: _YUE_HE_CAI_39,
+  5120: _YUE_HE_CAI_39,
+  YUE_HE_CAI_49: _YUE_HE_CAI_49,
+  1121: _YUE_HE_CAI_49,
+  XING_CAI_3: _XING_CAI_3,
+  2108: _XING_CAI_3,
+  XING_CAI_4: _XING_CAI_4,
+  2109: _XING_CAI_3,
+  BIN_GUO: _BIN_GUO,
+  1102: _BIN_GUO
+}
+
+export const CMD = {
+  SEARCH: {
+    URL: 'https://api.taiwanlottery.com/TLCAPIWeB/Lottery/Lotto649Result',
+    METHOD: 'get',
+    QUERY: {
+      month: '2025-05',
+      pageNum: 1,
+      pageSize: 20
+    }
+  },
+  LAST_NUMBER: {
+    URL: 'https://api.taiwanlottery.com/TLCAPIWeB/Lottery/LastNumber',
+    METHOD: 'get'
+  }
+}
+export default CMD
